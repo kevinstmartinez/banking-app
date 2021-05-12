b4d419793ade949c8190ea8d14cb85ea28706e1d
kevin stiven moreno martinez <root1@Mac-mini-de-kevin.local>
2021-05-11 20:59:56 -0500

    remove submodule app

--- src/index.js
+++ src/index.js
@@ -17,15 +17,7 @@ app.listen(port, ()=>{
 app.use(require('./routes/index.routes'))
 app.use('/api/accounts', require('./routes/account.routes'))
 app.use(require('./routes/client.routes'))
-<<<<<<< HEAD
-app.use('/payments', require('./routes/payment.routes'))
-app.use('/services', require('./routes/service.routes'))
-app.use('/transfers', require('./routes/transfer.routes'))
-
-app.use('/auth', require('./routes/auth.routes'))
-=======
 app.use('/api/payments', require('./routes/payment.routes'))
 app.use('/api/services', require('./routes/service.routes'))
 app.use('/api/transfers', require('./routes/transfer.routes'))
 app.use('/api/auth', require('./routes/auth.routes'))
->>>>>>> refactor/features
