48d24b8790f155dc4e288eb34848964f21db13f7
David Mosquera <al.mosqueradavid@gmail.com>
2021-05-11 22:21:31 -0500

    Modify from backend in other machine

--- src/database.js
+++ src/database.js
@@ -1,29 +1,32 @@
-require('dotenv').config()
-const mysql = require('mysql')
-const { promisify } = require('util')
+require("dotenv").config();
+const mysql = require("mysql2");
+const { promisify } = require("util");
 
-let host = process.env.DB_HOST
-let user = process.env.DB_USER
-let password = process.env.DB_PASSWORD
-let database_name = process.env.DB_DATABASE
+let host = process.env.DB_HOST;
+let user = process.env.DB_USER;
+let password = process.env.DB_PASSWORD;
+let database_name = process.env.DB_DATABASE;
 
 const database = {
   host: host,
   user: user,
   password: password,
-  database: database_name
-}
-const pool = mysql.createPool(database)
+  database: database_name,
+};
+const pool = mysql.createPool(database);
 
-pool.getConnection((err, conn)=>{
-  if(err){
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') console.error('database connection lost')
-    if(err.code === 'ER_CON_COUNT_ERROR') console.error('database has to many connection')
-    if(err.code === 'ECONNREFUSED') console.error('database connection was refused')
+pool.getConnection((err, conn) => {
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST")
+      console.error("database connection lost");
+    if (err.code === "ER_CON_COUNT_ERROR")
+      console.error("database has to many connection");
+    if (err.code === "ECONNREFUSED")
+      console.error("database connection was refused");
   }
-  if(conn) conn.release()
-  console.log('DB is connected')
-  return
-})
-pool.query = promisify(pool.query)
-module.exports = pool
+  if (conn) conn.release();
+  console.log("DB is connected");
+  return;
+});
+pool.query = promisify(pool.query);
+module.exports = pool;
