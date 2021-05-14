40f1e5783c7c65deb75add6347ffe04648eed5d6
NicolasRomeroRobayo <nromeror5@ucentral.edu.co>
2021-05-13 20:50:52 -0500

    apapi testing register added

--- cypress/integration/banking-app.spec.js
+++ cypress/integration/banking-app.spec.js
@@ -1,8 +1,25 @@
-describe('First test', () =>{
-  let a = 1+1
-  let b = 2
+describe('Test Proyecto', () =>{
+  
+  it('register test', () =>{
+    const item={"dni": 1193543225,"email": "nicolasRomero@gmail.com","name_lastname": "Nicolas Romero Robayo","username": "NicolasRomeroRobayo1","pass": "password1","passConfirm": "password1","id_bank": 1}
+    cy.request('POST','http://localhost:4001/api/auth/register',item)
+  })
 
-  it('The sum works', () =>{
-    expect(a===b).to.equal(true)
+  it('login test', () =>{
+    const item={"username": "NicolasRomeroRobayo","pass":"password"}
+    cy.request('POST', 'http://localhost:4001/api/auth/login',item)
+  })
+
+  /*it('payment test', () =>{
+    const item={"invoice_amount": 60000,"number_payment_references": "97728744170","id_service": 31}
+    cy.request({
+      Method: 'POST',
+      URL:'http://localhost:4001/api/payments/payment',item,auth: {bearer: PaymentResponse}
+    })
+    
+  })*/
+  
+  it('tranfer test', () =>{
+
   })
 })
